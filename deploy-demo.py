503f374f4b6f2d58b273cb9017eb5be5e7ed53c2
Christopher Nuland <cnuland@localhost.localdomain>
2020-04-26 01:21:41 -0400

    huh

--- deploy-demo.py
+++ deploy-demo.py
@@ -5,4 +5,4 @@ for i in range(1, services+1): # Create the specified amount of services
     # Use template to set the amount of services and individual identities of the app demo
     print(commands.getstatusoutput("oc process -f app-deploy.yaml -p SERVICES={} -p IDENTITY={} | oc create -f -".format(services, i))[1])
     # Create a service for the newly created deployment
-    print(commands.getstatusoutput("oc expose deployment/mesh-demo-{}".format(i, i))[1])
+    print(commands.getstatusoutput("oc expose deployment/mesh-demo-{}".format(i))[1])
